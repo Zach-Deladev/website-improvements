52458f236fb8e48e344021bf15a7a905c9847f5d
Zach Del <zach.delapenha.developer@gmail.com>
2023-04-14 07:00:46 +0100

    8th commit, added next and previous buttons to gallery.

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,36 +1,73 @@
-// Function to create gallery
-
 const gallery = document.getElementById("gallery");
 const popup = document.getElementById("popup");
 const imgselected = document.getElementById("imgselect");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("previous");
 
-const imgindex = [1,2,3,4,5,6,]
+// create array for image indexs 
+const imgindex = [1, 2, 3, 4, 5, 6];
 
-const selectedindex = null;
+// initialize current image index
+let currentImageIndex;
+
+//  open and close popup functions
+function openPopup(i) {
+    popup.style.transform = "translateY(0)";
+    imgselected.src = `../img/g-${i}.jpg`;
+  
+} 
+
+function closePopup() {
+    popup.style.transform = "translateY(-100%)";
+    imgselected.src = "";
+    
+}
 
 
+//  dynamically create images and add them to the galleryImg div
 imgindex.forEach(i => {
     const image = document.createElement("img");
     image.src = `../img/g-${i}.jpg`;
-    image.alt = `gallery image ${i}`;
     image.classList.add("galleryImg");
 
-
     image.addEventListener("click", () => {
-        popup.style.transform = "translateY(0)";
-        imgselected.src = `../img/g-${i}.jpg`;
-        imgselected.alt = `gallery image ${i}`;
+        currentImageIndex = i;
+        openPopup(i);
+    });
 
-    })
-        
-     gallery.appendChild(image);
+    gallery.appendChild(image);
+});
 
+
+// next and previous buttons functionality
+function showNextImage() {
+    if (currentImageIndex < imgindex.length) {
+        currentImageIndex++;
+    } else {
+        currentImageIndex = 1;
+    }
+    openPopup(currentImageIndex);
 }
-)
 
-popup.addEventListener("click", () => {
-    popup.style.transform = "translateY(-100%)";
-    popup.src = "";
-    popup.alt = "";
+function showPrevImage() {
+    if (currentImageIndex > 1) {
+        currentImageIndex--;
+    } else {
+        currentImageIndex = imgindex.length;
+    }
+    openPopup(currentImageIndex);
+}
 
-})
+
+// event listeners for the popup and buttons 
+
+popup.addEventListener("click", closePopup);
+
+nextBtn.addEventListener("click", (event) => {
+    event.stopPropagation();
+    showNextImage();
+});
+prevBtn.addEventListener("click", (event) => {
+    event.stopPropagation();
+    showPrevImage();
+});
